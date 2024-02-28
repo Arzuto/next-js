49a9453a3820f2ad4556f87bf44ae70ee08e6f63
Arya <aryarzt@gmail.com>
2024-02-28 08:04:25 +0700

    routing

--- components/header/index.js
+++ components/header/index.js
@@ -1,4 +1,18 @@
+import Link from "next/link"
+
 import styles from './styles.module.css'
 export default function Header() {
-  return <div className={styles.header}>Header</div>
-  }
+  return <div className={styles.header}>
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="profile">Profile</Link>
+      </li>
+      <li>
+        <Link href="users">User</Link>
+      </li>
+    </ul>
+  </div>
+}
