993dd32aeeb1c239739f32398601a2026deca92a
Arya <aryarzt@gmail.com>
2024-03-04 20:26:04 +0700

    dynamic image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Layout from "@/layout";
 import { useEffect } from "react"
+import Image from "next/image"
+import dynamic from "next/dynamic"
+
+const LayoutComponent = dynamic(() => import("@/layout"))
 
 export default function Main() {
   useEffect(() => {
@@ -10,9 +14,10 @@ export default function Main() {
   })
  return (
   <div>
-    <Layout metaTitle="Home" metaDescription="Ini adalah halaman menu utama">
+    <LayoutComponent metaTitle="Home" metaDescription="Ini adalah halaman menu utama">
       <p>Home</p>
-    </Layout> 
+      <Image src="/strawberry.png" width={400} height={400} alt="next strawberry image" />
+    </LayoutComponent> 
   </div>
  );
 }
