37fd772b1cb6de919931de88acb8519a912563e5
Arya <aryarzt@gmail.com>
2024-03-08 01:11:25 +0700

    custom hook

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -11,25 +11,18 @@ import {
   CardFooter,
   Heading,
   Text,
-  Button
+  Button,
+  Spinner
 } from '@chakra-ui/react'
 import { useEffect,useState } from "react"
 import { useRouter } from "next/router"
+import { useQueries } from "@/hooks/useQueries"
 
 const LayoutComponent = dynamic(() => import("@/layout"))
 
 export default function Notes() {
+  const { data, isLoading } = useQueries({ prefixUrl: 'https://paace-f178cafcae7b.nevacloud.io/api/notes' });  
   const router = useRouter();
-  const [notes,setNotes] = useState();  
-  useEffect(() => {
-    async function fetchingData(){
-      const res = await fetch("https:paace-f178cafcae7b.nevacloud.io/api/notes")
-      const listNotes = await res.json()
-      setNotes(listNotes)
-
-    }
-    fetchingData()
-  },[])
 
   const HandleDelete = async (id) => {
     try {
@@ -44,6 +37,8 @@ export default function Notes() {
       }
     } catch (error) {}
    }
+
+
  return (
   <>
     <LayoutComponent metaTitle="Notes" metaDescription="Ini adalah halaman Notes">
@@ -51,34 +46,47 @@ export default function Notes() {
         <Flex justifyContent="end">
           <Button colorScheme="blue" onClick={() => router.push('/notes/add')}>Add Button</Button>
         </Flex>
-        <Flex>
-        <Grid templateColumns='repeat(3,1fr)' gap={5}>
-          {
-            notes?.data?.map((item) => (
-              <GridItem>
-                <Card>
-                  <CardHeader>
-                    <Heading>{item?.title}</Heading>
-                  </CardHeader>
-                  <CardBody>
-                    <Text>{item?.description}</Text>
-                  </CardBody>
-                  <CardFooter justify="space-between" flexWrap="wrap">
-                     <Button onClick={() => router.push(`/notes/edit/${item?.id}`)} flex="1" variant="ghost">
-                      Edit
-                     </Button>
-                     <Button onClick={() => HandleDelete(item?.id)} flex="1" colorScheme="red">
-                      Delete
-                     </Button>
-                    </CardFooter>
-                </Card>
-              </GridItem>
-            ))
-          }
-        </Grid>
-      </Flex>
+        {
+          isLoading ? (
+            <Flex align="center" justify="center">
+            <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+            />
+          </Flex>
+          ) : (
+            <Flex>
+            <Grid templateColumns='repeat(3,1fr)' gap={5}>
+              {
+                data?.data?.map((item) => (
+                  <GridItem>
+                    <Card>
+                      <CardHeader>
+                        <Heading>{item?.title}</Heading>
+                      </CardHeader>
+                      <CardBody>
+                        <Text>{item?.description}</Text>
+                      </CardBody>
+                      <CardFooter justify="space-between" flexWrap="wrap">
+                         <Button onClick={() => router.push(`/notes/edit/${item?.id}`)} flex="1" variant="ghost">
+                          Edit
+                         </Button>
+                         <Button onClick={() => HandleDelete(item?.id)} flex="1" colorScheme="red">
+                          Delete
+                         </Button>
+                        </CardFooter>
+                    </Card>
+                  </GridItem>
+                ))
+              }
+            </Grid>
+          </Flex>
+          )
+        }
     </Box>
-
     </LayoutComponent> 
   </>
  )
