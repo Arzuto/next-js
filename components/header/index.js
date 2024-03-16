416bf5e0e92db9ab073f0aa9958a7f9906389b81
Arya <aryarzt@gmail.com>
2024-03-16 20:56:33 +0700

    state management with context

--- components/header/index.js
+++ components/header/index.js
@@ -7,16 +7,19 @@ import { useQueries } from "@/hooks/useQueries"
 import { useMutation } from "@/hooks/useMutation"
 import { useRouter } from "next/router"
 import Cookies from "js-cookie"
+import { useContext } from "react"
+import { UserContext } from "@/context/userContext"
 
 export default function Header() {
+  const userData = useContext(UserContext)
   const router = useRouter()
   const { mutate } = useMutation()
-  const { data } =  useQueries({
-    prefixUrl:"https://paace-f178cafcae7b.nevacloud.io/api/user/me",
-    headers:{
-      'Authorization': `Bearer ${Cookies.get('user_token')}`
-    }
-  })
+  // const { data } =  useQueries({
+  //   prefixUrl:"https://paace-f178cafcae7b.nevacloud.io/api/user/me",
+  //   headers:{
+  //     'Authorization': `Bearer ${Cookies.get('user_token')}`
+  //   }
+  // })
 
   const HandleLogout = async() => {
     const response = await mutate({
@@ -54,7 +57,7 @@ export default function Header() {
       <li>
       <Menu>
   <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-    {data?.data?.name}
+    {userData?.name}
   </MenuButton>
   <MenuList>
     <MenuItem onClick={() => HandleLogout()}>Log Out</MenuItem>
